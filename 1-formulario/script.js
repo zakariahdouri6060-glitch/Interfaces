7d5bef5f9787871a233f51cecfb5cdc5270d90ea
zakariahdouri6060-glitch <zakariahdouri6060@gmail.com>
2026-09-24 20:01:41 +0200

    correjer_escrito añadir_js

---
+++ 1-formulario/script.js
@@ -0,0 +1,47 @@
+console.log("Hola mundo 😁");
+console.warn("Dont do that");
+console.error("Error did it");
+
+var hola = 'Saludo';
+let quetal = "Saludo 2";
+const comoEstas = 'Bien 1';
+
+console.log("ANA" + hola + " " + quetal + " " + comoEstas);
+
+saludarBien();
+
+function saludarBien(x, y, z) {
+    console.log(x);
+    console.log(hola)
+}
+
+const header = document.getElementsByTagName('h1');
+
+const container = document.getElementsByClassName('container');
+
+const formulario = document.getElementById('formulario');
+
+const rellenaForm = document.getElementById('rellenaForm');
+
+const nombre = document.getElementById('name');
+
+const surName = document.getElementById('surname');
+
+const sexo = document.getElementById('sex');
+
+
+console.log(header);
+
+console.log(container);
+
+console.log(formulario);
+
+console.log(rellenaForm);
+
+console.log(nombre);
+
+console.log(surName);
+
+console.log(sexo);
+
+
